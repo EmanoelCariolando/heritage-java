cdc3c51bfa79375ec691cc4677f2233d70c3084d
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-28 21:16:54 -0300

    Solved exercise

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,59 @@
-import entities.Account;
-import entities.BusinessAccount;
-import entities.SavingAccount;
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        Account acc = new Account(123, "Manel", 1000.0);
-        Account baac = new BusinessAccount(123, "Manel", 450.0, 500.0);
+        List<Employee> employeeList = new ArrayList<>();
 
-        // UPCASTING
 
-        Account acc1 = baac;
-        Account acc2 = new BusinessAccount(143, "Lulu", 500.0, 600.0);
-        Account acc3 = new SavingAccount(122,"Mcacao", 1000.0, 0.05);
+        System.out.print("Enter the number of employees: ");
+        int n = sc.nextInt();
 
-        // DOWNCASTING
+        for(int i = 0; i < n; i++){
 
-        BusinessAccount baac1 = (BusinessAccount) acc2;
-        baac1.loan(100.0);
+            System.out.printf("Employee #%d data: ", i + 1);
+            System.out.print("Outsourced (y/n)? ");
+            char quest = sc.next().charAt(0);
 
-        // BusinessAccount acc5 = (BussinessAccount)acc3;
-        if (acc3 instanceof BusinessAccount){
-            BusinessAccount acc5 = (BusinessAccount) acc3;
-            acc5.loan(100.0);
-            System.out.println("Loaned 100$ to " + acc5.getHolder());
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+
+            System.out.print("Hours: ");
+            Integer hours = sc.nextInt();
+
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+
+            if (quest != 'y') {
+                Employee employee = new Employee(name, hours, valuePerHour);
+                employeeList.add(employee);
+            } else {
+                System.out.print("Addiditional Charge: ");
+                double additional = sc.nextDouble();
+                Employee secondEmployee = new OutsourcedEmployee(name, hours, valuePerHour, additional);
+                employeeList.add(secondEmployee);
+            }
+
         }
 
-        if (acc3 instanceof SavingAccount){
-            SavingAccount acc6 = (SavingAccount) acc3;
-            acc6.uptadeBalance();
-            System.out.println("Updated balance for " + acc6.getHolder());
+        System.out.println("PAYMENTS: ");
+
+        for (Employee emp : employeeList) {
+          System.out.printf("%s - $ %.2f \n",emp.getName(), emp.payament());
         }
 
-    }
+        sc.close();
+
+ }
 }
